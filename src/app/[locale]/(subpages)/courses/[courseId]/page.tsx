7cf670a2c87b16b82ee922113095e597442688a3
moatasim <mo32000a@gmail.com>
2025-01-29 12:15:39 +0200

    initial course page

--- src/app/[locale]/(subpages)/courses/[courseId]/page.tsx
+++ src/app/[locale]/(subpages)/courses/[courseId]/page.tsx
@@ -6,6 +6,8 @@ import Image from 'next/image'
 import altImg from "../../../../../../public/images/course.jpg"
 import Loader from '@/components/Loader/Loader'
 import Testimonials from '@/components/Testimonials/Testimonials'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendar, faChevronDown, faClock, faLocationPin } from '@fortawesome/free-solid-svg-icons'
 
 const CourseId = ({
     params,
@@ -27,12 +29,122 @@ const CourseId = ({
             {course?.title}
         </MainHeading>
         <div className="container">
+          <div className={styles.fixed}>
             <div className={styles.img}>
-                <Image loading='lazy' src={course.src} alt={`${course?.title} `}></Image>
+              <Image loading='lazy' src={course.src} alt={`${course?.title} `}></Image>
             </div>
+            
+            <div className={styles.overview}>
+              <ul className={styles.basicUl}>
+                <li>
+                  <span><FontAwesomeIcon icon={faCalendar} /></span>
+                  <h5>25-01-2025</h5>
+                </li>
+                <li>
+                  <span><FontAwesomeIcon icon={faLocationPin} /></span>
+                  <h5>Online</h5>
+                </li>
+                <li>
+                  <span><FontAwesomeIcon icon={faClock} /></span>
+                  <h5>6 Weeks</h5>
+                </li>
+              </ul>
+              <ul className={styles.more}>
+                <li>
+                  <span>Weekly Lecture:</span>
+                  <p>One 3-hour session covering core concepts</p>
+                </li>
+                <li>
+                  <span>Weekly Tutorial:</span>
+                  <p>One 1-hour session each week for in-depth discussions on advanced topics</p>
+                </li>
+                <li>
+                  <span>Weekly One-to-One Meetings:</span>
+                  <p>One 1-hour session available upon request for personalized guidance or deeper exploration of topics</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className={styles.scroll}>
+            <div className={styles.description}>
+              <h2>Desciption</h2>
+              <p>Welcome to the Introduction to Cancer Biology course! This course is structured to give participants a deep understanding of cancer biology, exploring everything from fundamental principles to advanced research topics. Each week, we will cover different aspects of cancer biology, from cellular mechanisms to therapeutic strategies.</p>
+            </div>
+            <div>
+              This course aims to equip you with knowledge and insights that will enable you to understand and interpret cancer biology research effectively. The course materials, assignments, and interactive sessions are designed to ensure a solid grasp of key topics and the latest developments in the field.
+            </div>
+            <div>
+              <h2>
+                Who Can Join the Course?
+              </h2>
+              <ul className={styles.more}>
+                  <li>
+                    <span>Undergraduate and Graduate Students:</span>
+                    <p>Ideal for those studying biomedical sciences, biotechnology, or related fields.</p>
+                  </li>
+                  <li>
+                    <span>Early Career Researchers:</span>
+                    <p>Suitable for researchers new to cancer biology or those transitioning into this field.</p>
+                  </li>
+                  <li>
+                    <span>Enthusiastic Learners:</span>
+                    <p>Anyone with a passion for understanding the complexities of cancer at the cellular level.</p>
+                  </li>
+                </ul>
+                <p>
+                  No prior experience in cancer research is required, though a basic understanding of Molecular Biology will be helpful!
+                </p>
+            </div>
+            <div>
+              <h2>Course Content</h2>
+              <ol className={styles.content}>
+                <li>
+                  <h3>
+                    <span>Introduction to Cancer </span><FontAwesomeIcon icon={faChevronDown} />
+                  </h3>
+                  <ul>
+                    <li>What is cancer?</li>
+                    <li>Basic cell biology of cancer development</li>
+                    <li>Differences between normal cells and cancer cells</li>
+                    <li>Introduction to tumor progression</li>
+                  </ul>
+                </li>
+                <li>
+                  <h3>
+                    <span>Basics of Cell Biology and the Cell Cycle </span><FontAwesomeIcon icon={faChevronDown} />
+                  </h3>
+                  <ul>
+                    <li>Introduction to cell structure and function </li>
+                    <li>Overview of the cell cycle and its regulation</li>
+                    <li>How uncontrolled cell cycle leads to cancer</li>
+                  </ul>
+                </li>
+                <li>
+                  <h3>
+                    <span>Tumor Types and Origins </span><FontAwesomeIcon icon={faChevronDown} />
+                  </h3>
+                  <ul>
+                    <li>Origin of tumors from normal tissues</li>
+                    <li>Classification of tumors: Benign vs. Malignant</li>
+                    <li>Histopathology and architecture of tumors</li>
+                    <li>Epithelial origin of carcinomas</li>
+                  </ul>
+                </li>
+                <li>
+                  <h3>
+                    <span>Hallmarks of Cancer </span><FontAwesomeIcon icon={faChevronDown} />
+                  </h3>
+                  <ul>
+                    <li>The "Hallmarks of Cancer" framework by Hanahan and Weinberg</li>
+                    <li>Key traits of cancer cells: sustaining proliferative signaling, evading growth suppressors, resisting cell death, enabling replicative immortality, and more</li>
+                  </ul>
+                </li>
+              </ol>
+            </div>
+          </div>
         </div>
         <Testimonials lo={locale} parentEl='courses' />
-        <Loader></Loader>
+        {/* <Loader></Loader> */}
       </section>
   )
 }
--- src/components/CoursesCards/allCourses.ts
+++ src/components/CoursesCards/allCourses.ts
@@ -1,6 +1,7 @@
 import { StaticImageData } from "next/image";
 
-import image1 from "../../../public/images/course.jpg"
+import image1 from "../../../public/images/IntroductionToCancerBiology.jpg"
+import image2 from "../../../public/images/course.jpg"
 
 export type Course = {
     id: string,
@@ -24,91 +25,91 @@ const allCourses : Course[] = [
         title: "Advanced Cancer Biology",
         category: "Advanced Biology",
         price: "Free",
-        src: image1
+        src: image2
     },
     {
         id: "3",
         title: "Immune Oncology",
         category: "Advanced Biology",
         price: "Coming Soon",
-        src: image1
+        src: image2
     },
     {
         id: "4",
         title: "Brain Tumors and Glial Cell Biology",
         category: "Advanced Biology",
         price: "Upon Request",
-        src: image1
+        src: image2
     },
     {
         id: "5",
         title: "Basics of Colorectal Cancer (CRC)",
         category: "Advanced Biology",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     },
     {
         id: "6",
         title: "Circular RNA (CircRNA) Biology",
         category: "Advanced Biology",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     },
     {
         id: "7",
         title: "RNA Sequencing",
         category: "Data Analysis and Technologies",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     },
     {
         id: "8",
         title: "Single-Cell RNA Sequencing",
         category: "Data Analysis and Technologies",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     },
     {
         id: "9",
         title: "Spatial Transcriptomics",
         category: "Data Analysis and Technologies",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     },
     {
         id: "10",
         title: "R Programming for Bioinformatics",
         category: "Data Analysis and Technologies",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     },
     {
         id: "11",
         title: "DNA Replication",
         category: "Basics",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     },
     {
         id: "12",
         title: "DNA Structure",
         category: "Basics",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     },
     {
         id: "13",
         title: "DNA Transcription",
         category: "Basics",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     },
     {
         id: "14",
         title: "Introduction to Bioinformatics",
         category: "Basics",
         price: "200.00$ USD",
-        src: image1
+        src: image2
     }
 ]
 
