2c3b383217fffae888bc03a3ee975053ebd45c0d
moatasim <mo32000a@gmail.com>
2025-02-09 09:30:45 +0200

    BUILT

--- src/app/[locale]/(subpages)/courses/[courseId]/page.tsx
+++ src/app/[locale]/(subpages)/courses/[courseId]/page.tsx
@@ -56,6 +56,7 @@ const CourseId = ({
     const {courseId} = use(params)
     const course: Course = allCourses.find((ele) => ele.id === courseId) || {
         id: "999",
+        enrollType: "",
         title: "",
         category: "",
         price: "",
@@ -69,7 +70,6 @@ const CourseId = ({
           },
             heading: "heading",
             description: "description",
-            why: ["String"],
             content: [
               {
                 button: "string",
