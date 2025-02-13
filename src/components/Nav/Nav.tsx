d2be43b4abb5fc3e30c2d954d8905f6803810ce2
moatasim <mo32000a@gmail.com>
2025-02-13 11:45:31 +0200

    BUILT

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -30,9 +30,9 @@ const Nav = ({
                 <li>
                     <Link onClick={() => dispatch(toggleShowSideNav())} className={pathname.split("/")[2] === "courses" ? styles.active : ""} href={'/courses'}>{t("1")}</Link>
                 </li>
-                <li>
+                {/* <li>
                     <Link onClick={() => dispatch(toggleShowSideNav())} className={pathname === `/${lo}/publications` ? styles.active : ""} href={'/publications'}>{t("2")}</Link>
-                </li>
+                </li> */}
                 <li>
                     <Link onClick={() => dispatch(toggleShowSideNav())} className={pathname.split("/")[2] === "blog" ? styles.active : ""} href={'/blog'}>{t("3")}</Link>
                 </li>
