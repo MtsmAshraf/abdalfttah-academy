5a49c672ec5147cf8c877f9be9f2a766be9ba0b0
moatasim <mo32000a@gmail.com>
2025-03-25 17:36:08 +0200

    trying to add social links imgs courses page 2 STATIC

--- src/app/[locale]/(subpages)/courses/[courseId]/layout.tsx
+++ src/app/[locale]/(subpages)/courses/[courseId]/layout.tsx
@@ -8,8 +8,8 @@ import { usePathname } from 'next/navigation';
 // const course = allCourses.find(course => course.id === )
 
 export const metadata: Metadata = {
-    title: "Abdalfattah Academy",
-    description: "Abdalfattah Academy official website",
+    title: "Abdalfttah Academy",
+    description: "Abdalfttah Academy official website",
     openGraph: {
         title:"title",
         url: "https://www.abdalfttah-academy.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fworkshop.cf931c5a.jpeg&w=640&q=75",
